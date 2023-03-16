24c65f67b7ac7774b186ce69891dfc167d49e57a
[your_name] <kazuhiro_natori@ca-adv.co.jp>
2023-03-16 18:27:06 +0900

    #1 鎖の全体を再制作

--- docs/projects/chainGeometry/chain.js
+++ docs/projects/chainGeometry/chain.js
@@ -3,27 +3,37 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer({
-  antialias: true
-});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff, 1);
 document.body.appendChild(renderer.domElement);
 
 
-// マテリアルを定義
-const chainMaterial = new THREE.MeshStandardMaterial({
-  color: 0x999999,
-  roughness: 0.5,
-  metalness: 0.8
-});
-
-
 // シーンを作成
 function buildScene() {
-  const chainRadius = 0.5;
-  const chainLength = 6;
+  const chainRadius = 0.3;
+  const chainLength = 4;
   const chainSegments = 16;
+  const leftCylinderTopX = -2;
+  const cylinderMarginX = 3;
+  const cylinderTopY = 0;
+
+  const rightCylinderTopX = leftCylinderTopX + cylinderMarginX;
+  const cylinderBottomY = cylinderTopY + chainLength;
+  const leftCylinderBottomX = rightCylinderTopX;
+  const rightCylinderBottomX = leftCylinderTopX + cylinderMarginX;
+
+  const topCurveEdgeX = rightCylinderTopX - (cylinderMarginX / 2);
+  console.log(topCurveEdgeX);
+  // const topCurveEdgeX = 0.3;
+  const topCurveEdgeY = 4;
+
+
+  const chainMaterial = new THREE.MeshStandardMaterial({
+    color: 0x999999,
+    roughness: 0.5,
+    metalness: 0.8
+  }); 
 
   const cylinderGeometry = new THREE.CylinderGeometry(
     chainRadius,
@@ -32,46 +42,65 @@ function buildScene() {
     chainSegments
   );
 
-  const chainMaterial = new THREE.MeshStandardMaterial({ color: 0x808080 });
-
-  // 左側の円柱
+  // Left cylinder
   const leftCylinderMesh = new THREE.Mesh(cylinderGeometry, chainMaterial);
-  leftCylinderMesh.position.set(-2, chainLength / 2, 0);
+  leftCylinderMesh.position.set(leftCylinderTopX, cylinderTopY, 0);
 
-  // 右側の円柱
+  // Right cylinder
   const rightCylinderMesh = new THREE.Mesh(cylinderGeometry, chainMaterial);
-  rightCylinderMesh.position.set(2, chainLength / 2, 0);
+  rightCylinderMesh.position.set(rightCylinderTopX, cylinderTopY, 0);
 
-  // カーブの始点と終点
-  const bottomCurveStart = new THREE.Vector3(-2, 0, 0);
-  const bottomCurveEnd = new THREE.Vector3(2, 0, 0);
+  // Top curve start and end points
+  const topCurveStart = new THREE.Vector3(leftCylinderTopX, cylinderTopY, 0);
+  const topCurveEdge = new THREE.Vector3(topCurveEdgeX, topCurveEdgeY, 0);
+  const topCurveEnd = new THREE.Vector3(rightCylinderTopX, cylinderTopY, 0);
 
-  // カーブ
-  const bottomCurvePoints = [
-    bottomCurveStart,
-    new THREE.Vector3(0, -2, 0),
-    bottomCurveEnd,
+  // Top curve
+  const topCurvePoints = [
+    topCurveStart,
+    topCurveEdge,
+    topCurveEnd
   ];
-  const curve = new THREE.CatmullRomCurve3(bottomCurvePoints);
-  const curveGeometry = new THREE.TubeGeometry(
-    curve,
+  const topCurve = new THREE.CatmullRomCurve3(topCurvePoints);
+
+  // Bottom curve
+  const bottomCurvePoints = topCurvePoints.slice().reverse();
+  const bottomCurve = new THREE.CatmullRomCurve3(bottomCurvePoints);
+
+  // Rotate top curve 180 degrees
+  topCurve.points.forEach((point) => {
+    point.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI);
+  });
+
+  // Tube geometry
+  const geometry = new THREE.TubeGeometry(
+    topCurve,
+    topCurvePoints.length * 4,
+    chainRadius,
+    chainSegments,
+    false
+  );
+
+  // Tube mesh
+  const mesh = new THREE.Mesh(geometry, chainMaterial);
+
+  // Bottom curve mesh
+  const bottomCurveGeometry = new THREE.TubeGeometry(
+    bottomCurve,
     bottomCurvePoints.length * 4,
     chainRadius,
     chainSegments,
     false
   );
-  const curveMesh = new THREE.Mesh(curveGeometry, chainMaterial);
+  const bottomCurveMesh = new THREE.Mesh(bottomCurveGeometry, chainMaterial);
 
-  scene.add(leftCylinderMesh);
-  scene.add(rightCylinderMesh);
-  scene.add(curveMesh);
+  // Add objects to scene 
+
+  
+  scene.add(leftCylinderMesh, rightCylinderMesh, topCurve, bottomCurveMesh);
 }
 
-
-
 buildScene();
-
-// ここまでシーン
 
 // ライティング
 // 環境光
@@ -87,19 +116,18 @@ spotLight.decay = 2;
 spotLight.distance = 500;
 scene.add(spotLight);
 
-
-// レンダリングする
+// レンダリンcグする
 function render() {
-    renderer.render(scene, camera);
-    spotLight.position.x = Math.sin(Date.now() * 0.001) * 10;
-    spotLight.position.z = Math.cos(Date.now() * 0.001) * 10;
-    requestAnimationFrame(render);
+renderer.render(scene, camera);
+const now = Date.now() * 0.001;
+spotLight.position.set(Math.sin(now) * 10, 10, Math.cos(now) * 10);
+requestAnimationFrame(render);
 }
 
 window.addEventListener('resize', () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 render();
