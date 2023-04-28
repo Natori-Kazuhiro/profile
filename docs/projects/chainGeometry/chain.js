b241c640f07312da2aad86d9ab780d34fe9be5e5
[your_name] <kazuhiro_natori@ca-adv.co.jp>
2023-04-28 12:27:00 +0900

    #12 いったん鎖っぽい形になった

--- docs/projects/chainGeometry/chain.js
+++ docs/projects/chainGeometry/chain.js
@@ -1,48 +1,49 @@
 // シーン、カメラ、レンダラーを設定
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// ライトを追加
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
 
 // 鎖のリンクを作成する関数
 const createChainLink = () => {
   const geometry = new THREE.TorusGeometry(1, 0.3, 16, 100);
-  const material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+  const material = new THREE.MeshPhongMaterial({ color: 0xaaaaaa });
   return new THREE.Mesh(geometry, material);
 };
 
+
 // 鎖のリンクを配置し、回転を設定
-const numLinks = 10;
+const numLinks = 50;
 const chainLinks = [];
-const linkDistance = 1.85;
-const linkSize = 2; // 鎖のサイズ
+const linkSize = 5;
+const linkDistance = linkSize / 3;
 
 for (let i = 0; i < numLinks; i++) {
   const link = createChainLink();
 
   let yOffset = 0;
-  let zOffset = 0;
 
   if (i > 0) {
     const prevLink = chainLinks[i - 1];
-    yOffset = prevLink.position.y - linkDistance * Math.cos(prevLink.rotation.z) - (linkSize / 2);
-    zOffset = prevLink.position.z - linkDistance * Math.sin(prevLink.rotation.z);
+    yOffset = prevLink.position.y - linkDistance;
   }
 
   link.position.y = yOffset;
-  link.position.z = zOffset;
-
-  link.rotation.x = (i % 2) * Math.PI / 2;
-  link.rotation.z = i * Math.PI / 2;
+  link.rotation.y = (i + 1) * Math.PI / 2;
 
   scene.add(link);
   chainLinks.push(link);
 }
 
 
+
 // カメラの位置を設定
-camera.position.z = 20;
+camera.position.z = 50;
 camera.lookAt(scene.position);
 
 // レンダリング
