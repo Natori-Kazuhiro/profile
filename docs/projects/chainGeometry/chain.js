54092ebb2c1be8b11836bf0a3840784a00d2538f
[your_name] <kazuhiro_natori@ca-adv.co.jp>
2023-06-06 18:19:01 +0900

    #12 3D空間上でも鎖に見えるように修正

--- docs/projects/chainGeometry/chain.js
+++ docs/projects/chainGeometry/chain.js
@@ -3,7 +3,7 @@ const scene = new THREE.Scene();
 
 // カメラの位置を設定
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 20;
+camera.position.z = 30;
 camera.lookAt(scene.position);
 
 // レンダラーを設定
@@ -12,61 +12,78 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // ライトを追加
-const light = new THREE.AmbientLight(0xffffff);
-scene.add(light);
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
 
-// 鎖のリンクを作成する関数
+// 部屋のような点光源
+const pointLight = new THREE.PointLight(0xffffff, 200, 60);
+scene.add(pointLight);
+
 const createChainLink = () => {
   const path = new THREE.CatmullRomCurve3([
-    new THREE.Vector3(    0,     0,   0),
-    new THREE.Vector3(  0.7,     0,   0),
-    new THREE.Vector3(    1,     0,   0),
-    new THREE.Vector3( 1.25,   0.5,   0),
-    new THREE.Vector3(    1,     1,   0),
-    new THREE.Vector3(  0.7,     1,   0),
-    new THREE.Vector3(    0,     1,   0),
-    new THREE.Vector3( -0.7,     1,   0),
-    new THREE.Vector3(   -1,     1,   0),
-    new THREE.Vector3(-1.25,   0.5,   0),
-    new THREE.Vector3(   -1,     0,   0),
-    new THREE.Vector3( -0.7,     0,   0),
-    new THREE.Vector3(    0,     0,   0)
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(0.7, 0, 0),
+    new THREE.Vector3(1, 0, 0),
+    new THREE.Vector3(1.25, 0.5, 0),
+    new THREE.Vector3(1, 1, 0),
+    new THREE.Vector3(0.7, 1, 0),
+    new THREE.Vector3(0, 1, 0),
+    new THREE.Vector3(-0.7, 1, 0),
+    new THREE.Vector3(-1, 1, 0),
+    new THREE.Vector3(-1.25, 0.5, 0),
+    new THREE.Vector3(-1, 0, 0),
+    new THREE.Vector3(-0.7, 0, 0),
+    new THREE.Vector3(0, 0, 0),
   ]);
 
   const geometry = new THREE.TubeGeometry(path, 64, 0.2, 8, false);
-  const material = new THREE.MeshPhongMaterial({ color: 0xaaaaaa });
+  const material = new THREE.MeshPhongMaterial({
+    color: 0xaaaaaa,
+    metalness: 1.0,
+    roughness: 0.5,
+    emissive: 0x111111,
+  });
 
   return new THREE.Mesh(geometry, material);
 };
 
-// 鎖のリンクを配置し、回転を設定
-const numLinks = 10;
+const numLinks = 50;
 const chainLinks = [];
-const linkSize = 10;
-const linkDistance = linkSize / numLinks;
+const linkSize = 1;
+const linkDistance = 1.6;
 
 for (let i = 0; i < numLinks; i++) {
   const link = createChainLink();
-  const yOffset = i * linkDistance * 2;
+  const yOffset = i * linkDistance;
 
-  const linkPositionX = i % 2 === 0 ? 0 : -0.5;
-  link.position.set(linkPositionX, yOffset, 0);
+  const linkPositionX = 0;
+  const linkPositionY = yOffset;
 
+  link.position.set(linkPositionX, linkPositionY, 0);
+  link.rotation.z = Math.PI / 2;
+
+  // 偶数の場合の処理
   if (i % 2 === 0) {
-    link.rotation.z = Math.PI / 2;
-  } else {
     link.rotation.y = Math.PI / 2;
-    link.rotation.z = Math.PI / 2;
+
+    // Adjust link's position
+    link.position.x = -0.5;
+    link.position.z = -0.5;
   }
 
   scene.add(link);
   chainLinks.push(link);
 }
 
-
-// レンダリング
 function animate() {
   requestAnimationFrame(animate);
+
+  // Update camera rotation around Y-axis
+  camera.position.x = 30 * Math.sin(Date.now() * 0.001);
+  camera.position.z = 30 * Math.cos(Date.now() * 0.001);
+  camera.lookAt(scene.position);
+
   renderer.render(scene, camera);
 }
+
 animate();
